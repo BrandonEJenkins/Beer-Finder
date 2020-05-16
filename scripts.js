43d56979b3ca97fb8544816675bcfe71e1c02708
BrandonEJenkins <brandonejenkins@gmail.com>
2020-05-15 23:41:24 -0400

    began pseudo coding in js file

---
+++ scripts.js
@@ -0,0 +1,34 @@
+/*      Beer Finder Application JS      */
+
+// Pseudo Code
+    
+    // Ajax call to beer APIs and use 'GET' request method to query URL for data
+
+    // Grab HTML elements using jQuery for use in JS ($('#elementID')
+
+    // Declare variable to store user beer history (in an array?)
+    // Create input text area for user beer search
+    // Create ajax call to beer API to fetch beer info
+    // Get text area html element
+    // Append / prepend recent beer search history
+    // Define number of list items to show in search history
+    // Declare variable to store api key
+    // Declare variabel to store url
+    // Create random beer recommendation function
+    // Declare variable that stores function to create new list item when response received from url
+    // Ajax call to get img of beer search results
+    // Ajax call to get images of beer recommendations
+    // Ajax call to get brief description of beer from search results
+    // Ajax call to get brief description of beer recommendations based on beer search results
+    // Variable that stores function to create new <p> tag with .text('data' + value) based on search resutls
+    // Variable that stores function to create new <p> tag with .text('data' + value) based on randomized recommendation
+    // Variable to store function to append new search results to search history
+
+
+    // Create event listeners:
+        // Beer search button
+        // Sign-up button
+        // Create account button
+        // Skip this step button
+        // Go to profile button
+    
