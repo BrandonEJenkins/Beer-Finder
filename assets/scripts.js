1a2fcb581d2e5cf29742caa33b60644f633239c6
M3D5 <pablohdzjr@gmail.com>
2020-05-30 09:22:11 -0500

    final before presentation

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,62 +1,69 @@
 /*      Beer Finder Application JS      */
 
+$(document).ready(function () {
+  var userBrandSearchInput;
+  var responseBreweryCity;
+  var responseBreweryState;
 
+  var stateLocalStorage = window.localStorage.getItem(
+    "userStateDropdownSelection"
+  );
+  var brandLocalStorage = window.localStorage.getItem("userBrandSearchInput");
 
+  userStateDropdownSelection = stateLocalStorage;
+  userBrandSearchInput = brandLocalStorage;
 
-$( document ).ready(function () {
+  getBreweryDetails();
 
+  $(".stateBtn").change(function (event) {
+    event.preventDefault();
 
-    var userBrandSearchInput;
-    var responseBreweryCity;
-    var responseBreweryState;
+    $(".cardDiv").empty();
 
-    var stateLocalStorage = window.localStorage.getItem("userStateDropdownSelection");
-    var brandLocalStorage = window.localStorage.getItem("userBrandSearchInput");
+    userStateDropdownSelection = $("#inputState option:selected").text();
+    userBrandSearchInput = $("#brandSearchInput").val();
 
-    userStateDropdownSelection = stateLocalStorage;
-    userBrandSearchInput = brandLocalStorage;
+    window.localStorage.setItem(
+      "userStateDropdownSelection",
+      userStateDropdownSelection
+    );
+    window.localStorage.setItem("userBrandSearchInput", userBrandSearchInput);
 
     getBreweryDetails();
-    
-    
-    $('.brandBtn').click(function (event) {
-        
-        
-        event.preventDefault();
-        
-        $('.cardDiv').empty();
-        
-        userStateDropdownSelection = $('#inputState option:selected').text();
-        userBrandSearchInput = $('#brandSearchInput').val();
-        
-        window.localStorage.setItem("userStateDropdownSelection", userStateDropdownSelection);
-        window.localStorage.setItem("userBrandSearchInput", userBrandSearchInput);
-        
-        getBreweryDetails();
+  });
 
-    })
+  $(".brandBtn").click(function (event) {
+    event.preventDefault();
 
-    
-    function getBreweryDetails() {
-        
+    $(".cardDiv").empty();
 
-        var queryURL = "https://api.openbrewerydb.org/breweries?by_name=" + userBrandSearchInput + "&by_state=" + userStateDropdownSelection + "&sort=+name&per_page=15";
-        
-        $.ajax({
-            url: queryURL,
-            methoed: 'GET',
-        }).then(function (response) {
+    userBrandSearchInput = $("#brandSearchInput").val();
 
-            console.log(response.length);
-            console.log(response);
+    window.localStorage.setItem("userBrandSearchInput", userBrandSearchInput);
 
-            for (let i =0; i < response.length; i++) {
+    getBreweryDetails();
+  });
 
+  function getBreweryDetails() {
+    var queryURL =
+      "https://api.openbrewerydb.org/breweries?by_name=" +
+      userBrandSearchInput +
+      "&by_state=" +
+      userStateDropdownSelection +
+      "&sort=+name&per_page=15";
 
-                responseBreweryCity = response[i].city;
-                responseBreweryState = response[i].state;
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response.length);
+      console.log(response);
 
-                $('.cardDiv').append(`
+      for (let i = 0; i < response.length; i++) {
+        responseBreweryCity = response[i].city;
+        responseBreweryState = response[i].state;
+
+        $(".cardDiv").append(`
                 
                     <div class="card">
                         <div class="card-divider">
@@ -76,43 +83,36 @@ $( document ).ready(function () {
 
                 `);
 
-                getCovidReport();
-            }
+        getCovidReport();
+      }
+    });
+  }
 
+  function getCovidReport() {
+    var settings = {
+      async: true,
+      crossDomain: true,
+      url:
+        "https://covid-19-statistics.p.rapidapi.com/reports?region_province=" +
+        responseBreweryState +
+        "&iso=USA&region_name=US&city_name=" +
+        responseBreweryCity +
+        "&q=US%20" +
+        responseBreweryState,
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "covid-19-statistics.p.rapidapi.com",
+        "x-rapidapi-key": "aa9315e3c9msh7e70db338b431dbp1dae55jsn19c615063e3f",
+      },
+    };
 
-        });
+    $.ajax(settings).done(function (response) {
+      console.log(response);
+      console.log(JSON.stringify(response));
 
-    }
-
-
-    function getCovidReport() {
-
-
-        var settings = {
-            "async": true,
-            "crossDomain": true,
-            "url": "https://covid-19-statistics.p.rapidapi.com/reports?region_province=" + responseBreweryState + "&iso=USA&region_name=US&city_name=" + responseBreweryCity + "&q=US%20" +responseBreweryState,
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-host": "covid-19-statistics.p.rapidapi.com",
-                "x-rapidapi-key": "aa9315e3c9msh7e70db338b431dbp1dae55jsn19c615063e3f"
-            }
-        }
-
-        $.ajax(settings).done(function (response) {
-
-            console.log(response);
-            console.log(JSON.stringify(response));
-
-
-            $('.cardDiv').append(`
+      $(".cardDiv").append(`
                 <p>No. of confirmed Covid cases in state: ${response.data[0].confirmed}</p>
             `);
-
-
-        });
-
-
-    }
-
+    });
+  }
 });
